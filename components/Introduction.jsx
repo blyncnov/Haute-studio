c913b646033ed02d611596c8a9175d9a5cb8aedc
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-07-05 19:27:52 +0100

    Haute studio web-app v2.6

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -14,14 +14,14 @@ const Introduction = () => {
             <h1>Let’s Introduce About Myself</h1>
             <p>
               Beginning blessed second a creepeth. Darkness wherein fish years
-              good air whose after seed appear midst evenin
+              good air whose after seed appear midst evenin .
               <br />
               <br />
               Beginning blessed second a creepeth. Darkness wherein fish years
               good air whose after seed appear midst evenin appear void give
-              third bearing divide one so
+              third bearing divide one so .
             </p>
-            <button>Hire Me </button>
+            <button>Connect with me </button>
           </div>
         </div>
       </div>
